158ecde5a7d6151fc58389c2be4afbf5b89f736f
Dan4yk228 <danyakolesnik629@gmail.com>
2024-01-31 00:30:01 +0200

    2

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,11 @@ const gulp        = require('gulp');
 const browserSync = require('browser-sync');
 const sass = require('gulp-sass')(require('sass'));
 const rename = require("gulp-rename");
+<<<<<<< Updated upstream
+=======
+const cleanCSS = require('gulp-clean-css');
+const autoprefixer 
+>>>>>>> Stashed changes
 
 // Static server
 gulp.task('server', function() {
@@ -16,6 +21,17 @@ gulp.task('server', function() {
 gulp.task('styles', function(){
     return gulp.src("src/scss/*.+(scss|sass)")
 		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+<<<<<<< Updated upstream
+=======
+		.pipe(rename({
+			prefix: "",
+			suffix: ".min",
+		}))
+		.pipe(autoprefixer({
+			cascade: false
+		}))
+		.pipe(cleanCSS({compatibility: 'ie8'}))
+>>>>>>> Stashed changes
 		.pipe(gulp.dest("src/css"))
 		.pipe(browserSync.stream());
 })
@@ -25,4 +41,8 @@ gulp.task('watch', function(){
 	gulp.watch("src/*.html").on("change", browserSync.reload);
 })
 
+<<<<<<< Updated upstream
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'))
+=======
+gulp.task('default', gulp.parallel('watch', 'server', 'styles'))
+>>>>>>> Stashed changes
