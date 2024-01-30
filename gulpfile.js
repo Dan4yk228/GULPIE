b7ec0ccbf062be8844b8dd71c703441d6d80b102
Dan4yk228 <danyakolesnik629@gmail.com>
2024-01-30 23:33:29 +0200

    1

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp        = require('gulp');
 const browserSync = require('browser-sync');
+const sass = require('gulp-sass')(require('sass'));
+const rename = require("gulp-rename");
 
 // Static server
 gulp.task('server', function() {
@@ -12,5 +14,15 @@ gulp.task('server', function() {
 
 
 gulp.task('styles', function(){
-    return gulp.src("src/scss/*.+(scss|sass"))
+    return gulp.src("src/scss/*.+(scss|sass)")
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(gulp.dest("src/css"))
+		.pipe(browserSync.stream());
 })
+
+gulp.task('watch', function(){
+	gulp.watch("src/scss/*.+(scss|sass)", gulp.parallel("styles"));
+	gulp.watch("src/*.html").on("change", browserSync.reload);
+})
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles'))
